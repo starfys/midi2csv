a5e50b7cd467a7cf76cf07ba72341104a4e10532
Luke Stanley <lukexorz@users.noreply.github.com>
2016-01-30 06:23:54 -0600

    Update gen_level.py

--- gen_level.py
+++ gen_level.py
@@ -15,8 +15,10 @@ def compare(event1, event2):
             return -1   
         return 0
 
-tempo = 500000.0
+
 pattern = midi.read_midifile(sys.argv[1])
+tempo = 500000.0
+resolution = pattern.resolution * 1.0
 output_file = open(sys.argv[2], 'w+')
 output_file_raw = open(sys.argv[2]+".raw", 'w+')
 print >>output_file_raw, pattern
@@ -38,13 +40,20 @@ for event in events:
     elif type(event) == midi.NoteOffEvent:
         notes.append((turned_on[event.data[0]], event.data[0], event.tick - turned_on[event.data[0]]))
     elif type(event) == midi.SetTempoEvent:
-        notes.append((event.tick, (((event.data[0]*65536)+event.data[1]*256+event.data[2])/1000000.0), 0))
+        notes.append((event.tick, (((event.data[0]*65536)+event.data[1]*256+event.data[2])), 0))
 
 notes = sorted(notes,cmp=compare)
-
-print >>output_file, len(notes)
+final_notes = []
+last_time = 0.0
+last_ticks = 0
 for note in notes:
     if note[2] == 0:
-        print >>output_file, note[0], "%.2f" % round(note[1],2), note[2]
-    else:
-        print >>output_file, note[0], note[1], note[2]
+        tempo = note[1]
+        continue
+    next_time = last_time + ((tempo * (note[0] - last_ticks)) / (resolution * 1000000))
+    final_notes.append((next_time, note[1], (tempo * note[2]) / (resolution * 1000000)))
+    last_ticks = note[0]
+    last_time = next_time
+print >>output_file, len(final_notes)
+for note in final_notes:
+        print >>output_file, "%.2f" % round(note[0],2), note[1], "%.2f" % round(note[2], 2)
